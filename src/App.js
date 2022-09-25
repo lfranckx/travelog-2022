c19eda545fb71a0238c2db49eea2d3da98c330bd
Bowie <lfranckx@thinkful.com>
2022-09-25 15:51:40 -0500

    install router-dom

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-import './App.css';
+import './App.scss';
+import { Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
       </header>
+      <Routes>
+
+      </Routes>
     </div>
   );
 }
---
+++ src/Routes/Homepage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default function Homepage() {
+    return (
+        <main id='home' className='main'>
+            <div className='title'>
+                <h2>Share authentic Travel Stories</h2>
+                <h3>Connect with other travelers and inspire</h3>
+            </div>
+            <h3 className='italic'>Stories,</h3>
+        </main>
+    )
+}
